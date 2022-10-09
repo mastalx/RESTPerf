82ca60f28c02fca691ee44beb53873c9760d95d2
Philipp Rohr <philipp.rohr@protonmail.ch>
2022-10-09 22:37:13 +0200

    small refactoring

--- src/main/java/ch/tie/perf/scenario/Measurement.kt
+++ src/main/java/ch/tie/perf/scenario/Measurement.kt
@@ -3,13 +3,11 @@ package ch.tie.perf.scenario
 class Measurement(val name: String, val id: Long, val timestamp: Long, val duration: Long) : Comparable<Measurement> {
 
     override fun compareTo(other: Measurement): Int {
-        var retVal = name.compareTo(other.name)
-        if (retVal != 0) {
-            return retVal
+        val nameComp = name.compareTo(other.name)
+        if (nameComp != 0) {
+            return nameComp
         }
-        retVal = timestamp.compareTo(other.timestamp)
-        return if (retVal != 0) {
-            retVal
-        } else duration.compareTo(other.duration)
+        val timeComp = timestamp.compareTo(other.timestamp)
+        return if (timeComp != 0) timeComp else duration.compareTo(other.duration)
     }
 }
